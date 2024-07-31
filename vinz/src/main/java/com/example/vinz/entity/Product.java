4a761cc79717ee0be95f0a5e3e7bfa42b15dc38a
eulerBM <eullerborgesdamotta155@gmail.com>
2024-07-30 22:51:30 -0300

    Models do produto

--- vinz/src/main/java/com/example/vinz/entity/Product.java
+++ vinz/src/main/java/com/example/vinz/entity/Product.java
@@ -2,6 +2,7 @@ package com.example.vinz.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Table(name = "product")
@@ -12,11 +13,16 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(unique = true)
     private UUID id_public;
 
+    @Column(length = 200, nullable = false)
     private String name;
 
+    @Column(length = 500, nullable = false)
     private String description;
 
-    private String
+    @Column(length = 50, nullable = false)
+    private LocalDateTime published_data;
+
 }

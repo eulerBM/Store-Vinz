4c5ec934da43a36d56db5cea0e4f488041243d83
eulerBM <eullerborgesdamotta155@gmail.com>
2024-10-16 19:34:03 -0300

    Adicionando a tela do produtos no front end, produtos publicados do proprio usuario

--- front-end/src/components/pages/MyPubli.js
+++ front-end/src/components/pages/MyPubli.js
@@ -1,20 +1,57 @@
 import NavBar from "../Forms/NavBar";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-function MyPubli () {
-    return(
+function MyPubli() {
+    const [products, setProducts] = useState([]);
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    const idPublic = userInfo ? userInfo.idPublic : null;
+
+    useEffect(() => {
+        if (idPublic) {
+            axios.get(`http://localhost:8080/products/meus_publicados/${idPublic}`)
+                .then(response => {
+                    setProducts(response.data);
+                })
+                .catch(error => {
+                    console.error("Erro ao buscar produtos:", error);
+                });
+        }
+    }, [idPublic]);
+
+    return (
         <div>
             <NavBar />
+            <div className="container">
+            <div className="row">
+                {products.map((product) => (
+                    <div key={product.id} className="col-md-4 mb-4">
+                        <div className="card" style={{ width: '18rem' }}>
+                            <img src={product.imageUrl} className="card-img-top" alt={product.name} />
+                            <div className="card-body">
+                                <h5 className="card-title">{product.name}</h5>
+                                <p className="card-text">
 
-            <h1>Meus Publicados</h1>
-
-
+                                {product.description
+                                    ? product.description.length > 40 
+                                        ? `${product.description.substring(0, 40)}...` 
+                                        : product.description
+                                    : 'Descrição não disponível'}
+                                    
+                                </p>
+                                <a href={`/products/get/${product.id_public}`} className="btn btn-primary">
+                                    Ver mais
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
+
         
-
-    )
-    
-
-
+    </div>
+    );
 }
 
 export default MyPubli;

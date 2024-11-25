8e11c063ca1d2413720b89fd62322bb5ab936d34
eulerBM <eullerborgesdamotta155@gmail.com>
2024-11-25 16:17:34 -0300

    Adicionando formatação mais adequada

--- front-end/src/components/pages/Home.js
+++ front-end/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import NavBar from "../Forms/NavBar";
 import { useState, useEffect } from 'react';
 import Carrinho from "../utils/Carrinho";
+import Pagination from "../utils/Pagination"
 import axios from 'axios';
 
 function Home() {
@@ -21,7 +22,6 @@ function Home() {
                 setProducts(response.data.products || []);
                 setTotalPages(response.data.totalPages || 1);
                 setPageAtual(response.data.currentPage)
-                activeCssInButtons(PageAtual)
                 
             }
         } catch (error) {
@@ -36,58 +36,7 @@ function Home() {
     useEffect(() => {
         fetchProducts();
     }, [PageAtual]);
-
-    function activeCssInButtons (pageAtualUser) {
-
-        const activeLinks = document.querySelectorAll('.page-link.active');
-
-        activeLinks.forEach(link => link.classList.remove('active'));
-
-        let links = document.getElementById(`pagination-${pageAtualUser}`);
-
-        if (links) {
-
-            links.classList.add("active");
-
-        } 
-    }
     
-    const pageViews = ({pageClick, proxima, voltar}) => {
-
-        if (pageClick > totalPages || PageAtual < 0) return;
-
-        if (pageClick){
-
-            setPageAtual(pageClick)  
-                
-            activeCssInButtons(PageAtual);
-
-        } 
-        else if ( proxima == true){
-
-            if (PageAtual >= totalPages -1){
-
-                setPageAtual(0)
-                activeCssInButtons(PageAtual);
-
-            } else {
-                
-                setPageAtual(PageAtual + 1)
-                activeCssInButtons(PageAtual);
-
-            }
-        }
-        else if (voltar == true){
-
-            setPageAtual(PageAtual - 1)
-
-            activeCssInButtons(PageAtual);
-
-        }
-    };
-
-
-
     return (
         <div>
             <NavBar />
@@ -129,32 +78,8 @@ function Home() {
 
             {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
 
-            <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <li class="page-item">
-                    <a class="page-link" aria-label="Previous" onClick={() => pageViews({voltar: true})}>
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                    </li>
 
-                    {totalPages > 0 ? (
-
-                        Array.from({ length: totalPages }, (_, index) => (
-                            <li className="page-item" key={index}>
-                                <a className="page-link" id={`pagination-${index}`} onClick={() => pageViews({ pageClick: index})}>{index +1}</a>
-                            </li>
-                        ))
-                    ) : (
-                        <p>Erro no sistema!</p>
-                    )}
-                                        
-                    <li class="page-item">
-                    <a class="page-link" aria-label="Next" onClick={() => pageViews({proxima: true})}>
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                    </li>
-                </ul>
-            </nav>
+            <Pagination totalPages_={totalPages} pageAtual_={PageAtual} onPageChange={setPageAtual} />
 
         </div>
     );
